be67000a25584573b06c93fc71f2b21149da25bb
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-04-28 21:48:46 +0700

    Update add-detail-jadwal.php

--- user/add-detail-jadwal.php
+++ user/add-detail-jadwal.php
@@ -5,7 +5,7 @@ header("Content-Type: application/json");
 include '../conn.php';
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    mysqli_query($db, "INSERT INTO `detail_jadwal`(`id_jampel`, `id_kelas`, `id_user`, `status`, `mapel` `ket`) VALUES ('$_POST[id_jampel]','$_POST[id_kelas]','$_POST[id_user]','$_POST[status]', '$_POST[mapel]', '$_POST[ket]')");
+    mysqli_query($db, "INSERT INTO `detail_jadwal`(`id_jampel`, `id_kelas`, `id_user`, `status`, `mapel`, `ket`) VALUES ('$_POST[id_jampel]','$_POST[id_kelas]','$_POST[id_user]','$_POST[status]', '$_POST[mapel]', '$_POST[ket]')");
             
     $response = array();
     $response['message'] = "Berhasil menyimpan";

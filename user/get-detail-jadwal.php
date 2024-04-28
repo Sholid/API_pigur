31fd11c73a8ebdeeed79f8825390d88221c7ac4c
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-04-28 19:14:15 +0700

    Update get-detail-jadwal.php

--- user/get-detail-jadwal.php
+++ user/get-detail-jadwal.php
@@ -3,7 +3,7 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 // include '../conn.php';
 require "../conn.php";
 header("Content-Type: application/json");
-$cek = "SELECT * FROM `detail_jadwal` WHERE id_jampel = '$_GET[id_jampel]'";
+$cek = "SELECT kelas.id_kelas, kelas.kelas, jampel.id_jampel, jampel.jam_ke, detail_jadwal.* FROM `detail_jadwal` JOIN kelas ON detail_jadwal.id_kelas = kelas.id_kelas JOIN jampel ON detail_jadwal.id_jampel = jampel.id_jampel WHERE detail_jadwal.id_jampel = '$_GET[id_jampel]'";
 $query = mysqli_query($db, $cek);
 $response = array();
 while ($result = mysqli_fetch_assoc($query)) {

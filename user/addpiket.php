ef5e98d74a49aafef6d14270a7ff5baa5e99cb15
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-04-29 10:26:19 +0700

    update

--- user/addpiket.php
+++ user/addpiket.php
@@ -1,17 +1,15 @@
 <?php
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-// include '../conn.php';
 header("Content-Type: application/json");
 include '../conn.php';
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    foreach(json_decode($_POST['tanggal']) as $tangggal) {
+    foreach(explode(";",$_POST['tanggal']) as $tangggal) {
         $sql = "INSERT INTO `jadwal`(`tanggal`) VALUES ('$tangggal')";
         $query = mysqli_query($db, $sql);
         if ($query) {
             $id_jadwal = mysqli_insert_id($db);
-            foreach(json_decode($_POST['guru']) as $guru) {
-                mysqli_query($db, "INSERT INTO `guru_piket`(`id_jadwal`, `id_user`, `is_koord`) VALUES ('$id_jadwal', '$guru->user_id', '$guru->is_koord')");
+            foreach($_POST['guru'] as $guru) {
+                mysqli_query($db, "INSERT INTO `guru_piket`(`id_jadwal`, `id_user`, `is_koord`) VALUES ('$id_jadwal', '$guru[user_id]', '$guru[is_koord]')");
             }
         }
     }
